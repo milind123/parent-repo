f6a63fff2f8488d4bcfe9c280a43b9caf7d72ba1
milind <milind123@gmail.com>
2023-04-24 22:00:48 -0400

    commetfs

--- src/main/java/com/milind/move/moveit/MoveitApplication.java
+++ src/main/java/com/milind/move/moveit/MoveitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoveitApplication {
 
 	public static void main(String[] args) {
+		System.err.println("Hello");
 		SpringApplication.run(MoveitApplication.class, args);
 	}
 
